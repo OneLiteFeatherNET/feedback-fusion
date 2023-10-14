09f05a576500c08767844e6ffc703962f32eaf77
Random <53190645+Randoooom@users.noreply.github.com>
2023-10-14 21:41:02 +0200

    feat: basic server

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,91 @@
  *
  */
 
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate lazy_static;
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate tracing;
+#[macro_use]
+extern crate utoipa;
+
+use axum::error_handling::HandleErrorLayer;
+use axum::http::StatusCode;
+use axum::{BoxError, Router, Server};
+use std::net::SocketAddr;
+use std::time::Duration;
+use tower::buffer::BufferLayer;
+use tower::limit::RateLimitLayer;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Config {
+    #[serde(default = "default_global_rate_limit")]
+    global_rate_limit: u64,
+}
+
+fn default_global_rate_limit() -> u64 {
+    10
+}
+
+lazy_static! {
+    pub static ref CONFIG: Config = envy::from_env::<Config>().unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    // init config
+    lazy_static::initialize(&CONFIG);
+
+    // init the tracing subscriber with the `RUST_LOG` env filter
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let (sender, receiver) = kanal::oneshot_async::<()>();
+
+    let address = SocketAddr::from(([0, 0, 0, 0], 8000));
+    tokio::spawn(async move {
+        Server::bind(&address)
+            .serve(router().into_make_service())
+            .with_graceful_shutdown(async move {
+                receiver.recv().await.ok();
+            })
+            .await
+            .unwrap();
+    });
+
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => {}
+        Err(error) => {
+            error!("Unable to listen for the shutdown signal: {}", error);
+        }
+    }
+
+    info!("Received shutdown signal... shutting down...");
+    sender.send(()).await.unwrap();
+}
+
+fn router() -> Router {
+    Router::new().layer(
+        ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    error!("Unhandled error occurred: {}", err),
+                )
+            }))
+            .layer(BufferLayer::new(1024))
+            // set the max requests per sec for all incoming calls
+            .layer(RateLimitLayer::new(
+                CONFIG.global_rate_limit.clone(),
+                Duration::from_secs(1),
+            ))
+            .layer(TraceLayer::new_for_http()),
+    )
 }
