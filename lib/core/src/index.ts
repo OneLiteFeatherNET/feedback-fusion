535ee1e2d06456730b3128b4a620e78dc362570b
Fritz Leo Ochsmann <53190645+Randoooom@users.noreply.github.com>
2024-01-05 21:23:50 +0100

    feat: most basic lib setup

--- lib/core/src/index.ts
+++ lib/core/src/index.ts
@@ -1,9 +1,34 @@
-import { SubmitFeedbackPromptResponseRequest, FeedbackPromptResponse } from "../../../bindings";
+import {
+  FeedbackPromptResponse,
+  SubmitFeedbackPromptResponseRequest,
+} from "../../../bindings";
 
+export class FeedbackFusionClient {
+  public baseURL: string;
 
-export async function submitResponse(options: SubmitFeedbackPromptResponseRequest): Promise<FeedbackPromptResponse> {
+  public constructor(baseURL: string, target: String) {
+    this.baseURL = `${baseURL}/v1/target/${target}/`;
+  }
 
+  /**
+   * Submit a new response to the prompt
+   * @param prompt {string} the target prompt
+   * @param responses {SubmitFeedbackPromptResponseRequest} the field responses
+   * @returns {Promise<FeedbackPromptResponse>}
+   */
+  public async submitResponse(
+    prompt: String,
+    responses: SubmitFeedbackPromptResponseRequest,
+  ): Promise<FeedbackPromptResponse> {
+    return await fetch(`${this.baseURL}/prompt/${prompt}/response`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(responses),
+    })
+      .then((response) => response.json() as Promise<FeedbackPromptResponse>)
+      .then((response: FeedbackPromptResponse) => response);
+      // TODO: handle errors
+  }
 }
 
 export * from "../../../bindings";
-
